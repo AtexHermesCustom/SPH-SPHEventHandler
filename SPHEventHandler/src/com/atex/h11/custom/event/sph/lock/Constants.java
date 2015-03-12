63b834ae66827cfac65a5b7cff30d2b2cabc5fc9
jp <jmariano@atex.com>
2015-03-12 13:09:51 +0800

    test only

--- SPHEventHandler/src/com/atex/h11/custom/event/sph/lock/Constants.java
+++ SPHEventHandler/src/com/atex/h11/custom/event/sph/lock/Constants.java
@@ -3,7 +3,7 @@ package com.atex.h11.custom.event.sph.lock;
 public class Constants {
 	// Logging
 	public static final String NAME_LOGGER 				= LockHandler.class.getName();
-	public static final String log4jConfig 				= "conf/sphlockhandler.log4j.properties";
+	public static final String log4jConfig 				= "conf/sphlockhandler.log4j.propertiess";
 	
 	// Entries in the configuration properties file. 
 	public static final String DefaultConfigurationFile	= "conf/sphlockhandler.properties";
